4f7857a67ef61bdd47a726eeb0d205952515ca19
Jamiebullock87 <jamiebullock1987@gmail.com>
2018-12-02 15:10:31 +0000

    finished adding firebase back end and storage to the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,20 +2,34 @@ import { Component } from '@angular/core';
 
 import { DataStorageService } from '../shared/data-storage.service';
 import { Response } from '@angular/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {}
 
   onStoreRecipes() {
     this.dataStorageService.storeRecipes()
-    .subscribe(
-      (response: Response) => {
-        console.log(response);
-      }
-    );
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+  onFetchRecipes() {
+    this.dataStorageService.fetchRecipes();
   }
 }
+
+
+// onGet() {
+//   this.serverService.getServers()
+//     .subscribe(
+//       (servers: any[]) => this.servers = servers,
+//       (error) => console.log(error)
+//     );
+// }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -9,5 +11,25 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put('https://jamies-recipe-app.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+  fetchRecipes() {
+    return this.http.get('https://jamies-recipe-app.firebaseio.com/recipes.json')
+      .pipe(map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              console.log(recipe);
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      ))
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
