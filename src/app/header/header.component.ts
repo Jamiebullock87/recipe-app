d14b04d49b4efbfb428b3559ef4ab217ff11c9d9
Jamiebullock87 <jamiebullock1987@gmail.com>
2018-12-02 14:41:36 +0000

    added put request functionality with firebase back-end

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
 
+import { DataStorageService } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onStoreRecipes() {
+    this.dataStorageService.storeRecipes()
+    .subscribe(
+      (response: Response) => {
+        console.log(response);
+      }
+    );
+  }
+}
