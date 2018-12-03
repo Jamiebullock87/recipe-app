c7b2d60e48c866ff95664cbeff350c31d84183ff
Jamiebullock87 <jamiebullock1987@gmail.com>
2018-12-03 15:31:05 +0000

    added shoppinglist and auth feature modules to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 // import { ErrorPageComponent } from './error-page/error-page.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
   // Each route mentioned here, each one will be a js object
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'shopping-list', component: ShoppingListComponent }
   // { path: 'not-found', component: ErrorPageComponent },
   // { path: '**', redirectTo: '/not-found', pathMatch: 'full' }
-  { path: 'signup', component: SignupComponent },
-  { path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
