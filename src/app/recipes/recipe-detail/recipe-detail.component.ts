de9d90e5a60d05f2d5bf27423c5804585e9b12db
Jamiebullock87 <jamiebullock1987@gmail.com>
2018-12-04 15:52:30 +0000

    fixed error in recipe-detail component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -41,4 +41,7 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.deleteRecipe(this.recipeId);
     this.router.navigate(['/recipes']);
   }
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
 }
